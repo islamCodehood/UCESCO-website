30ca655a149e39807c47f58b58fb12bcad13b9c8
Islam Sayed <islam.sayed8@gmail.com>
2021-05-08 14:03:10 +0200

    add intersection observer for counter

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-const countAction = () => {
-  const counters = document.querySelectorAll("p.counter");
-  
-  for (const counter of counters) {
-    let count = 0;
+const countAction = (impactSection) => {
+  if (!impactSection.classList.contains("done-counting")) {
+    const counters = document.querySelectorAll("p.counter");
+    for (const counter of counters) {
+      let count = 0;
       const number = counter.dataset.count;
       const countingInterval = setInterval(() => {
-        
         if (count == number) {
           stopCounting(countingInterval);
         } else {
-            count++;
-            counter.textContent = count
+          count++;
+          counter.textContent = count;
         }
       }, timeInterval(number));
+    }
   }
+  return
 };
 
 const stopCounting = (countingInterval) => {
@@ -21,16 +22,44 @@ const stopCounting = (countingInterval) => {
 };
 
 const timeInterval = (number) => {
-    if (number >= 500) {
-        return 1
-    } else {
-        return 15
+  if (number >= 5000) {
+      console.log("big" + number)
+    return 0.1;
+  } else if (number >= 500) {
+    console.log("med" + number)
+    return 1.5;
+  } else {
+    console.log(number)
+      return 15
+  }
+};
+
+detectIntersection = () => {
+
+    const impactSection = document.querySelector(".impact-section");
+    if ("IntersectionObserver" in window) {
+      const observer = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              countAction(impactSection);
+              entry.target.classList.add("done-counting");
+              observer.unobserve(entry.target);
+            }
+          });
+        },
+        { rootMargin: "0px 0px -300px 0px" }
+      );
+    
+      //Observe sections
+      observer.observe(impactSection);
     }
 }
 
+
 const initApp = () => {
   console.log("started");
-  countAction();
+  detectIntersection();
 };
 
 window.addEventListener("load", initApp);
